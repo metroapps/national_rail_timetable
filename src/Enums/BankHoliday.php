26fd4f24299ec984a13c246e30289393b15ad0e8
Michael Tsang <miklcct@gmail.com>
2022-04-27 20:47:15 +0100

    Bank holiday field is unused.
    Ref: https://www.railforums.co.uk/threads/bank-holiday-excluded-field-in-timetable-data-no-effect.230878/#post-5633762

--- src/Enums/BankHoliday.php
+++ src/Enums/BankHoliday.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 namespace Miklcct\NationalRailJourneyPlanner\Enums;
 
 use Miklcct\NationalRailJourneyPlanner\Models\Date;
-use function Safe\file_get_contents;
-use function Safe\json_decode;
 
 enum BankHoliday : string {
     case NONE = '';
@@ -13,20 +11,7 @@ enum BankHoliday : string {
     case GLASGOW = 'G';
 
     public function isActive(Date $date) : bool {
-        static $data = null;
-        if ($data === null) {
-            $data = json_decode(
-                file_get_contents(__DIR__ . '/../../resource/holidays.json')
-            );
-        }
-        return in_array(
-            match ($this) {
-                self::ENGLAND => 'EAW',
-                self::GLASGOW => 'SCT',
-                self::NONE => '',
-            }
-            , $data->{$date->__toString()}->regions ?? []
-            , true
-        );
+        // field has no effect, ref https://www.railforums.co.uk/threads/bank-holiday-excluded-field-in-timetable-data-no-effect.230878/#post-5633762
+        return false;
     }
 }
