97fda5d989466b6d9621e42182a79643a5cf4cbd
Michael Tsang <miklcct@gmail.com>
2022-10-12 18:41:35 +0100

    add function to get connection time

--- src/Models/Station.php
+++ src/Models/Station.php
@@ -23,6 +23,15 @@ class Station extends Location {
         $this->tocConnectionTimes = $tocConnectionTimes;
     }
 
+    public function getConnectionTime(string $from_toc, string $to_toc) : int {
+        foreach ($this->tocConnectionTimes as $interchange) {
+            if ($interchange->arrivingToc === $from_toc && $interchange->departingToc === $to_toc) {
+                return $interchange->connectionTime;
+            }
+        }
+        return $this->minimumConnectionTime;
+    }
+
     /** @var TocInterchange[] */
     #[ElementType(TocInterchange::class)]
     public readonly array $tocConnectionTimes;
