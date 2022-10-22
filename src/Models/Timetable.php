a1967ec24097bcedebc51bf787e02c4e59bb98b7
Michael Tsang <miklcct@gmail.com>
2022-10-22 22:05:38 +0100

    improve station sorting

--- src/Models/Timetable.php
+++ src/Models/Timetable.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Miklcct\NationalRailTimetable\Models;
 
 use Miklcct\NationalRailTimetable\Exceptions\UnreachableException;
+use function array_reverse;
 
 class Timetable {
     // this number must be greater than the maximum number of calls for a train
@@ -36,49 +37,78 @@ class Timetable {
                 <=> count($arrival_mode ? $b->precedingCalls : $b->subsequentCalls)
             )
         );
-        foreach ($calls as $call) {
-            foreach (array_keys($arrival_mode ? $call->origins : $call->destinations) as $portion) {
-                $order = [];
-                $i = 0;
-                foreach ($arrival_mode ? $call->precedingCalls : $call->subsequentCalls as $subsequent_call) {
-                    if (array_key_exists($portion, $arrival_mode ? $subsequent_call->origins : $subsequent_call->destinations)) {
-                        $current_station = $subsequent_call->call->location;
-                        if ($current_station instanceof LocationWithCrs && $current_station->getCrsCode() !== null) {
-                            $found = null;
-                            $old_i = $i;
-                            while (isset($stations[$i])) {
-                                if ($stations[$i]->getCrsCode() === $current_station->getCrsCode()) {
-                                    $found = $i++;
-                                    break;
+        if ($arrival_mode) {
+            $calls = array_reverse($calls);
+        }
+        while (array_filter($calls) !== []) {
+            foreach ($calls as &$call) {
+                if ($call !== null) {
+                    $destinations = $arrival_mode ? $call->origins : $call->destinations;
+                    $portions_remaining = count($destinations);
+                    foreach (array_keys($destinations) as $portion) {
+                        $order = [];
+                        $i = $arrival_mode ? count($stations) - 1 : 0;
+                        $found_one = false;
+                        foreach ($arrival_mode ? array_reverse($call->precedingCalls) : $call->subsequentCalls as $subsequent_call) {
+                            if (
+                                array_key_exists(
+                                    $portion,
+                                    $arrival_mode ? $subsequent_call->origins : $subsequent_call->destinations
+                                )
+                            ) {
+                                $current_station = $subsequent_call->call->location;
+                                if (
+                                    $current_station instanceof LocationWithCrs
+                                    && $current_station->getCrsCode()
+                                    !== null
+                                ) {
+                                    $found = null;
+                                    $old_i = $i;
+                                    while (isset($stations[$i])) {
+                                        if ($stations[$i]->getCrsCode() === $current_station->getCrsCode()) {
+                                            $found = $i;
+                                            $i += $arrival_mode ? -1 : 1;
+                                            $found_one = true;
+                                            break;
+                                        }
+                                        $i += $arrival_mode ? -1 : 1;
+                                    }
+                                    if (!$found) {
+                                        $i = $old_i;
+                                    }
+                                    $order[] = [$current_station, $found === null ? null : $found * self::MULTIPLIER];
                                 }
-                                ++$i;
                             }
-                            if (!$found) {
-                                $i = $old_i;
+                        }
+                        if (!$found_one && $stations !== []) {
+                            // current portion has no common calls with processed services
+                            // try another one first
+                            continue;
+                        }
+                        foreach ($order as $j => $item) {
+                            if ($item[1] !== null) {
+                                for ($k = $j - 1; $k >= 0 && $order[$k][1] === null; --$k) {
+                                    $order[$k][1] = $item[1] + (self::MULTIPLIER - 1 - $k) * ($arrival_mode ? 1 : -1);
+                                }
                             }
-                            $order[] = [$current_station, $found === null ? null : $found * self::MULTIPLIER];
                         }
-                    }
-                }
-                foreach ($order as $j => $item) {
-                    if ($item[1] !== null) {
-                        for ($k = $j - 1; $k >= 0 && $order[$k][1] === null; --$k) {
-                            $order[$k][1] = $item[1] - self::MULTIPLIER + 1 + $k;
+                        $max = count($stations);
+                        foreach ($order as &$item) {
+                            if ($item[1] === null) {
+                                $item[1] = $max++ * self::MULTIPLIER * ($arrival_mode ? -1 : 1);
+                            }
                         }
-                    }
-                }
-                $max = count($stations);
-                foreach ($order as &$item) {
-                    if ($item[1] === null) {
-                        $item[1] = $max++ * self::MULTIPLIER;
-                    }
-                }
-                unset($item);
+                        unset($item);
 
-                $new_stations = array_reduce(
-                    $order
-                    , static fn(array $carry, array $item) : array
-                        => $carry + [$item[1] => $item[0]]
+                        foreach ($order as $i => $item) {
+                            if ($i > 0) {
+                                assert(($order[$i - 1][1] <=> $order[$i][1]) === ($arrival_mode ? 1 : -1));
+                            }
+                        }
+
+                        $new_stations = array_reduce(
+                            $order
+                            , static fn(array $carry, array $item) : array => $carry + [$item[1] => $item[0]]
                             , array_combine(
                                 array_map(
                                     static fn(int $x) => $x * self::MULTIPLIER
@@ -87,11 +117,18 @@ class Timetable {
                                 , array_values($stations)
                             )
                         );
-                ksort($new_stations);
-                $stations = array_values($new_stations);
+                        ksort($new_stations);
+                        $stations = array_values($new_stations);
+                        --$portions_remaining;
+                    }
+                    if ($portions_remaining === 0) {
+                        $call = null;
+                    }
+                }
             }
+            unset($call);
         }
-        $stations = array_merge([$calls[0]->call->location], $stations);
+        $stations = array_merge([$board->calls[0]->call->location], $stations);
 
         $matrix = [];
 
