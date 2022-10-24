3f570265a492fbea76e259bdd0f26f8b298a1b24
Michael Tsang <miklcct@gmail.com>
2022-10-24 22:45:08 +0100

    change of set of pre-cache stations

--- bin/cache_boards.bash
+++ bin/cache_boards.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
 for station in \
-LBG CST BFR CTK WAE CHX VXH WAT VIC PAD MYB EUS STP KGX OLD MOG LST FST \
-BDS TCR ZFD \
-CLJ SRA BHM MAN LDS GLC
+LBG WAT VIC PAD EUS STP KGX LST ZFD \
+CLJ RDG SRA BHM MAN LDS GLC
 do
     "$(dirname "$0")"/show_departure_board.php "today 12:00" $station >/dev/null &
     "$(dirname "$0")"/show_departure_board.php --arrivals "today 12:00" $station >/dev/null &
